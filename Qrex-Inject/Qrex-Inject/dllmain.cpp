84f5c9e62570f5828e26d8d33506d79fd1464a5a
Gubbi <rasmusschubert123@gmail.com>
2019-05-04 20:00:25 +0200

    Removed unnecessary code

--- Qrex-Inject/Qrex-Inject/dllmain.cpp
+++ Qrex-Inject/Qrex-Inject/dllmain.cpp
@@ -8,42 +8,7 @@ ILuaInterface* qrex = NULL;
 HMODULE module = NULL;
 
 
-void addHook(std::string name, std::string identifier, CLuaFunction function)
-{
-	qrex->PushSpecial(SPECIAL_GLOB);
-	qrex->GetField(-1, "hook");
-	qrex->GetField(-1, "Add");
-	qrex->PushString(name.c_str(), name.length());
-	qrex->PushString(identifier.c_str(), identifier.length());
-	qrex->PushCFunction(function);
-	qrex->Call(3, 0);
-	qrex->Pop();
-	qrex->Pop();
-}
-
-void removeHook(std::string name, std::string identifier)
-{
-	qrex->PushSpecial(SPECIAL_GLOB);
-	qrex->GetField(-1, "hook");
-	qrex->GetField(-1, "Remove");
-	qrex->PushString(name.c_str(), name.length());
-	qrex->PushString(identifier.c_str(), identifier.length());
-	qrex->Call(2, 0);
-	qrex->Pop();
-	qrex->Pop();
-}
-
-int Main(lua_State * state)
-{
-	removeHook("Think", "RunLuaMenuInit");
-	// qrex->RunString("RunString", "", "print('fuckyou')", true, true);
-
-	// Place code above ^
-	return 0;
-}
-
-int DllInit()
-{
+int getClientState() {
 	HMODULE LuaShared_modhandle = GetModuleHandle("lua_shared.dll");
 	if (LuaShared_modhandle != NULL)
 	{
@@ -64,16 +29,25 @@ int DllInit()
 		MessageBox(NULL, "[Qrextomnia] Lua State not initialized", NULL, MB_OK);
 		return 1;
 	}
-	addHook("Think", "RunLuaMenuInit", Main);
 	return 0;
 }
+
+int Main(lua_State * state)
+{
+	getClientState();
+	// qrex->RunString("RunString", "", "print('fuckyou')", true, true);
+
+	// Place code above ^
+	return 0;
+}
+
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
 {
 	if (reason == DLL_PROCESS_ATTACH)
 	{
 		module = hModule;
-		CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)DllInit, NULL, NULL, NULL);
+		CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)Main, NULL, NULL, NULL);
 	}
 	else if (reason == DLL_PROCESS_DETACH)
 	{
